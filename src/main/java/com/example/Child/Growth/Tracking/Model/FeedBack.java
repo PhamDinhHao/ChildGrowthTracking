03c4fa92a67bc1193d7d5a07785a09bf60fa3694
PhamDinhHao <quangkhai336694@gmail.com>
2025-02-15 14:42:52 +0700

    add database

---
+++ src/main/java/com/example/Child/Growth/Tracking/Model/FeedBack.java
@@ -0,0 +1,41 @@
+package com.example.Child.Growth.Tracking.Model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+import com.example.Child.Growth.Tracking.ulti.ConsultationStatus;
+
+@Entity
+@Table(name = "feedback")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FeedBack {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+    
+    @Column(nullable = false)
+    private Integer rating;
+    
+    @Column
+    private String comment;
+    
+    @Column(name = "created_at", updatable = false)
+    private LocalDate createdAt;
+    
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDate.now();
+    }
+} 
+
+
+
